de47963078842b333716618d03ed795a532f361d
thaipokecard <thaipokecard@hotmail.com>
2021-11-16 16:40:28 +0700

    hw01

---
+++ HW01/HW01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include "card.cpp"
+
+using namespace std;
+
+class CardList{
+
+public:
+	Card* head;
+    CardList(){
+        /*
+         WRITE YOUR CODE HERE
+         */
+    }
+
+	void pop_back(){
+        /*
+         WRITE YOUR CODE HERE
+         */
+	}
+
+
+    void insert_back(string newItem){
+        /*
+         WRITE YOUR CODE HERE
+         */
+    }
+
+    void shuffle(int pos){ //1<=pos<=size-1
+        /*
+         WRITE YOUR CODE HERE
+         */
+    }
+};
