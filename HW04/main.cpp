fbb0d9060831f1786c314846265bc9fcbe56a181
unknown <cjakarin@gmail.com>
2021-12-08 20:46:25 +0700

    change empty() to isEmpty()

--- HW04/main.cpp
+++ HW04/main.cpp
@@ -23,20 +23,20 @@ int main()
         if(input[i]>='a'&&input[i]<='z')
             cout<<input[i];
         else{
-            if(st.empty()){
+            if(st.isEmpty()){
                 st.push(input[i]);
             }else{
                 if(input[i]=='('){
                     st.push(input[i]);
                 }else if(input[i]==')'){
-                    while(!st.empty()&&st.top()!='('){
+                    while(!st.isEmpty()&&st.top()!='('){
                         cout<<st.top();
                         st.pop();
                     }
-                    if(!st.empty())
+                    if(!st.isEmpty())
                         st.pop();
                 }else{
-                    while(!st.empty()&&st.top()!='('&&point(input[i])<=point(st.top())){
+                    while(!st.isEmpty()&&st.top()!='('&&point(input[i])<=point(st.top())){
                         cout<<st.top();
                         st.pop();
                     }
@@ -46,7 +46,7 @@ int main()
             }
         }
     }
-    while(!st.empty()){
+    while(!st.isEmpty()){
         cout<<st.top();
         st.pop();
     }
